09d831e7a4c7b982758419e26d66a46db217a697
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-01 07:58:30 +0000

    initial checkin

---
+++ trunk/salve-contract/src/main/java/salve/contract/ContractInstrumentor.java
@@ -0,0 +1,50 @@
+package salve.contract;
+
+import salve.BytecodeLoader;
+import salve.CannotLoadBytecodeException;
+import salve.InstrumentationException;
+import salve.Instrumentor;
+import salve.contract.impl.ClassInstrumentor;
+import salve.org.objectweb.asm.ClassReader;
+import salve.org.objectweb.asm.ClassWriter;
+
+public class ContractInstrumentor implements Instrumentor {
+
+	public byte[] instrument(String className, BytecodeLoader loader)
+			throws InstrumentationException {
+		// FIXME factor out these arg checks into an abstract instrumentor
+		if (loader == null) {
+			throw new IllegalArgumentException(
+					"Argument `loader` cannot be null");
+		}
+		if (className == null) {
+			throw new IllegalArgumentException(
+					"Argument `className` cannot be null");
+		}
+
+		className = className.trim();
+
+		if (className.length() == 0) {
+			throw new IllegalArgumentException(
+					"Argument `className` cannot be an empty");
+		}
+
+		try {
+			byte[] bytecode = loader.loadBytecode(className);
+			if (bytecode == null) {
+				throw new CannotLoadBytecodeException(className);
+			}
+
+			ClassReader reader = new ClassReader(bytecode);
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+			ClassInstrumentor inst = new ClassInstrumentor(writer);
+			reader.accept(inst, 0);
+
+			return writer.toByteArray();
+		} catch (Exception e) {
+			// TODO message
+			throw new InstrumentationException(e);
+		}
+	}
+
+}
---
+++ trunk/salve-contract/src/main/java/salve/contract/NotNull.java
@@ -0,0 +1,14 @@
+package salve.contract;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Target( { ElementType.PARAMETER, ElementType.METHOD })
+public @interface NotNull {
+
+}
