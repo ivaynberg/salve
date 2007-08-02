e3b879085a64e9d0fc64b4ddbec384970e933bb2
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-02 15:50:43 +0000

    apache headers

--- trunk/salve-maven2/src/main/java/salve/maven2/util/Directory.java
+++ trunk/salve-maven2/src/main/java/salve/maven2/util/Directory.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -33,12 +34,10 @@ public class Directory {
 	 */
 	public Directory(File dir) {
 		if (!dir.exists()) {
-			throw new IllegalArgumentException(
-					"Argument `dir` points to a location that does not exist");
+			throw new IllegalArgumentException("Argument `dir` points to a location that does not exist");
 		}
 		if (!dir.isDirectory()) {
-			throw new IllegalArgumentException(
-					"Argument `dir` is not a directory");
+			throw new IllegalArgumentException("Argument `dir` is not a directory");
 		}
 		this.dir = dir;
 	}
