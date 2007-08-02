4bedde2c6203217fe50d22442e7ed031a65e8e0a
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-02 23:59:26 +0000

    @GE0 @GT0 @LE0 @LT0 wip

--- trunk/salve/src/main/java/salve/util/asm/AsmUtil.java
+++ trunk/salve/src/main/java/salve/util/asm/AsmUtil.java
@@ -3,11 +3,65 @@ package salve.util.asm;
 import salve.org.objectweb.asm.Type;
 
 public class AsmUtil {
+	private static final String DOUBLEDESC = "Ljava/lang/Double;";
+	private static final String FLOATDESC = "Ljava/lang/Float;";
+	private static final String LONGDESC = "Ljava/lang/Long;";
+	private static final String INTEGERDESC = "Ljava/lang/Integer;";
+	private static final String SHORTDESC = "Ljava/lang/Short;";
+	private static final String BYTEDESC = "Ljava/lang/Byte;";
+	private static final String BOOLEANDESC = "Ljava/lang/Boolean;";
+	private static final String CHARDESC = "Ljava/lang/Character;";
+
 	private AsmUtil() {
 
+	}
+
+	public static boolean isDouble(Type type) {
+		return Type.DOUBLE == type.getSort() || "Ljava/lang/Double;".equals(type.getDescriptor());
+	}
+
+	public static boolean isFloat(Type type) {
+		return Type.FLOAT == type.getSort() || "Ljava/lang/Float;".equals(type.getDescriptor());
+	}
+
+	public static boolean isLong(Type type) {
+		return Type.LONG == type.getSort() || "Ljava/lang/Long;".equals(type.getDescriptor());
 	}
 
 	public static boolean isPrimitive(Type type) {
 		return type.getSort() != Type.OBJECT && type.getSort() != Type.ARRAY;
 	}
+
+	public static Type toPrimitive(Type type) {
+		int sort = type.getSort();
+		if (sort == Type.ARRAY) {
+			throw new IllegalArgumentException("Type `" + type.toString() + "` does not have a primitive counterpart");
+		}
+		if (sort == Type.OBJECT) {
+			String desc = type.getDescriptor();
+			if (DOUBLEDESC.equals(desc)) {
+				return Type.DOUBLE_TYPE;
+			} else if (FLOATDESC.equals(desc)) {
+				return Type.FLOAT_TYPE;
+			} else if (FLOATDESC.equals(desc)) {
+				return Type.FLOAT_TYPE;
+			} else if (INTEGERDESC.equals(desc)) {
+				return Type.INT_TYPE;
+			} else if (SHORTDESC.equals(desc)) {
+				return Type.SHORT_TYPE;
+			} else if (BYTEDESC.equals(desc)) {
+				return Type.BYTE_TYPE;
+			} else if (BOOLEANDESC.equals(desc)) {
+				return Type.BOOLEAN_TYPE;
+			} else if (CHARDESC.equals(desc)) {
+				return Type.CHAR_TYPE;
+			} else {
+				throw new IllegalStateException("Cannot convert type `" + desc + "` to its primitive counterpart`");
+			}
+		} else {
+			return type;
+		}
+
+	}
+
 }
--- trunk/salve/src/main/java/salve/util/asm/GeneratorAdapter.java
+++ trunk/salve/src/main/java/salve/util/asm/GeneratorAdapter.java
@@ -4,11 +4,9 @@ import salve.org.objectweb.asm.MethodVisitor;
 import salve.org.objectweb.asm.Opcodes;
 import salve.org.objectweb.asm.Type;
 
-public class GeneratorAdapter extends
-		salve.org.objectweb.asm.commons.GeneratorAdapter implements Opcodes {
+public class GeneratorAdapter extends salve.org.objectweb.asm.commons.GeneratorAdapter implements Opcodes {
 
-	public GeneratorAdapter(MethodVisitor mv, int access, String name,
-			String desc) {
+	public GeneratorAdapter(MethodVisitor mv, int access, String name, String desc) {
 		super(mv, access, name, desc);
 	}
 
@@ -19,41 +17,33 @@ public class GeneratorAdapter extends
 		} else {
 			int sort = value.getSort();
 			if (sort == Type.BOOLEAN) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Boolean", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Boolean", "TYPE", "Ljava/lang/Class;");
 			} else if (sort == Type.CHAR) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Char", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Char", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.BYTE) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Byte", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Byte", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.SHORT) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Short", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Short", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.INT) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Integer", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Integer", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.FLOAT) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Float", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Float", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.LONG) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Long", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Long", "TYPE", "Ljava/lang/Class;");
 			} else
 
 			if (sort == Type.DOUBLE) {
-				mv.visitFieldInsn(GETSTATIC, "java/lang/Double", "TYPE",
-						"Ljava/lang/Class;");
+				mv.visitFieldInsn(GETSTATIC, "java/lang/Double", "TYPE", "Ljava/lang/Class;");
 			} else {
 				mv.visitLdcInsn(value);
 			}
