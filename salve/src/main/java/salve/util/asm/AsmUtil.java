3330df5e695f02dc8e113750f11e1d2bcf7db59f
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-03 07:49:02 +0000

    asm utils tweaks

--- salve/src/main/java/salve/util/asm/AsmUtil.java
+++ salve/src/main/java/salve/util/asm/AsmUtil.java
@@ -16,26 +16,45 @@ public class AsmUtil {
 
 	}
 
+	public static boolean isByte(Type type) {
+		return Type.BYTE == type.getSort()
+				|| "Ljava/lang/Byte;".equals(type.getDescriptor());
+	}
+
 	public static boolean isDouble(Type type) {
-		return Type.DOUBLE == type.getSort() || "Ljava/lang/Double;".equals(type.getDescriptor());
+		return Type.DOUBLE == type.getSort()
+				|| "Ljava/lang/Double;".equals(type.getDescriptor());
 	}
 
 	public static boolean isFloat(Type type) {
-		return Type.FLOAT == type.getSort() || "Ljava/lang/Float;".equals(type.getDescriptor());
+		return Type.FLOAT == type.getSort()
+				|| "Ljava/lang/Float;".equals(type.getDescriptor());
+	}
+
+	public static boolean isInteger(Type type) {
+		return Type.INT == type.getSort()
+				|| "Ljava/lang/Integer;".equals(type.getDescriptor());
 	}
 
 	public static boolean isLong(Type type) {
-		return Type.LONG == type.getSort() || "Ljava/lang/Long;".equals(type.getDescriptor());
+		return Type.LONG == type.getSort()
+				|| "Ljava/lang/Long;".equals(type.getDescriptor());
 	}
 
 	public static boolean isPrimitive(Type type) {
 		return type.getSort() != Type.OBJECT && type.getSort() != Type.ARRAY;
 	}
 
+	public static boolean isShort(Type type) {
+		return Type.SHORT == type.getSort()
+				|| "Ljava/lang/Short;".equals(type.getDescriptor());
+	}
+
 	public static Type toPrimitive(Type type) {
 		int sort = type.getSort();
 		if (sort == Type.ARRAY) {
-			throw new IllegalArgumentException("Type `" + type.toString() + "` does not have a primitive counterpart");
+			throw new IllegalArgumentException("Type `" + type.toString()
+					+ "` does not have a primitive counterpart");
 		}
 		if (sort == Type.OBJECT) {
 			String desc = type.getDescriptor();
@@ -43,8 +62,8 @@ public class AsmUtil {
 				return Type.DOUBLE_TYPE;
 			} else if (FLOATDESC.equals(desc)) {
 				return Type.FLOAT_TYPE;
-			} else if (FLOATDESC.equals(desc)) {
-				return Type.FLOAT_TYPE;
+			} else if (LONGDESC.equals(desc)) {
+				return Type.LONG_TYPE;
 			} else if (INTEGERDESC.equals(desc)) {
 				return Type.INT_TYPE;
 			} else if (SHORTDESC.equals(desc)) {
@@ -56,7 +75,8 @@ public class AsmUtil {
 			} else if (CHARDESC.equals(desc)) {
 				return Type.CHAR_TYPE;
 			} else {
-				throw new IllegalStateException("Cannot convert type `" + desc + "` to its primitive counterpart`");
+				throw new IllegalStateException("Cannot convert type `" + desc
+						+ "` to its primitive counterpart`");
 			}
 		} else {
 			return type;
