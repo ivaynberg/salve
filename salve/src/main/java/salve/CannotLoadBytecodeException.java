0006945ebdbd1f2441e637e7655947bfc319868c
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-04 21:53:51 +0000

    tweaks

--- salve/src/main/java/salve/CannotLoadBytecodeException.java
+++ salve/src/main/java/salve/CannotLoadBytecodeException.java
@@ -4,4 +4,8 @@ public class CannotLoadBytecodeException extends RuntimeException {
 	public CannotLoadBytecodeException(String className) {
 		super("Cannot load bytecode for class " + className);
 	}
+
+	public CannotLoadBytecodeException(String className, Throwable e) {
+		super("Cannot load bytecode for class " + className, e);
+	}
 }
