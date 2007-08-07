ea1a9550fadff9a1a5e0427c8b6909a2279c870b
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-07 04:11:03 +0000

    introduced instrumentor monitor so we have some visibility into the instrumentation process

--- salve-depend-spring-inst/src/main/java/salve/spring/txn/TransactionalInstrumentor.java
+++ salve-depend-spring-inst/src/main/java/salve/spring/txn/TransactionalInstrumentor.java
@@ -3,19 +3,20 @@ package salve.spring.txn;
 import salve.BytecodeLoader;
 import salve.InstrumentationException;
 import salve.Instrumentor;
+import salve.InstrumentorMonitor;
 import salve.asmlib.ClassReader;
 import salve.asmlib.ClassWriter;
 
 public class TransactionalInstrumentor implements Instrumentor {
 
-	public byte[] instrument(String className, BytecodeLoader loader)
-			throws InstrumentationException {
+	public byte[] instrument(String className, BytecodeLoader loader,
+			InstrumentorMonitor monitor) throws InstrumentationException {
 
 		try {
 			final byte[] bytecode = loader.loadBytecode(className);
 			ClassReader reader = new ClassReader(bytecode);
 			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-			ClassInstrumentor inst = new ClassInstrumentor(writer);
+			ClassInstrumentor inst = new ClassInstrumentor(writer, monitor);
 			reader.accept(inst, 0);
 			return writer.toByteArray();
 		} catch (Exception e) {
