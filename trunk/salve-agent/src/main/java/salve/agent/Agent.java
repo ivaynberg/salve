1d94512b1a6dfacd5bbd93e5aa3dee0ff2389912
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2007-08-08 00:42:42 +0000

    javadoc

--- trunk/salve-agent/src/main/java/salve/agent/Agent.java
+++ trunk/salve-agent/src/main/java/salve/agent/Agent.java
@@ -16,6 +16,7 @@
  */
 package salve.agent;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
@@ -25,6 +26,7 @@ import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 
+import salve.InstrumentationException;
 import salve.Instrumentor;
 import salve.config.Config;
 import salve.config.PackageConfig;
@@ -34,44 +36,65 @@ import salve.loader.CompoundLoader;
 import salve.loader.MemoryLoader;
 import salve.monitor.NoopMonitor;
 
+/**
+ * Jvm agent that applies Salve instrumentors to classes as they are being
+ * loaded.
+ * <p>
+ * To configure add the following jvm parameter to the command line:
+ * <code>-javaagent:path-to-this-jar</code>
+ * </p>
+ * 
+ * @author ivaynberg
+ */
 public class Agent {
 	private static Instrumentation INSTRUMENTATION;
 
 	private static class Transformer implements ClassFileTransformer {
+		/** set of classloaders that were already used to look for salve config */
 		private final Set<ClassLoader> seenLoaders = new HashSet<ClassLoader>();
+
+		/** set urls from which a salve config was already loaded */
 		private final Set<URL> seenUrls = new HashSet<URL>();
+
+		/** salve configuration */
 		private final Config config = new Config();
 
+		/**
+		 * {@inheritDoc}
+		 */
 		public byte[] transform(ClassLoader loader, String className,
 				Class<?> classBeingRedefined,
 				ProtectionDomain protectionDomain, byte[] classfileBuffer)
 				throws IllegalClassFormatException {
 
+			// see if this is a new classloader, and if it is try to load any
+			// salve config files
 			if (!seenLoaders.contains(loader)) {
-				XmlConfigReader reader = new XmlConfigReader(loader);
-				try {
-					Enumeration<URL> urls = loader
-							.getResources("META-INF/salve.xml");
-
-					while (urls.hasMoreElements()) {
-						URL url = urls.nextElement();
-						if (!seenUrls.contains(url)) {
-							reader.read(url.openStream(), config);
-							seenUrls.add(url);
-						}
-					}
-				} catch (Exception e) {
-					throw new RuntimeException(
-							"Could not process salve configuration files", e);
-				}
+				mergeConfigs(loader);
 				seenLoaders.add(loader);
 			}
 
+			// instrument bytecode
+			return instrument(loader, className, classfileBuffer);
+		}
+
+		/**
+		 * Instruments class
+		 * 
+		 * @param loader
+		 *            class loader
+		 * @param className
+		 *            binary class name (eg salve/agent/Agent)
+		 * @param bytecode
+		 *            bytecode
+		 * @return instrumented bytecode
+		 */
+		private byte[] instrument(ClassLoader loader, String className,
+				byte[] bytecode) {
 			try {
 				final String name = className.replace("/", ".");
 				PackageConfig conf = config.getPackageConfig(name);
 				if (conf != null) {
-					byte[] bytecode = classfileBuffer;
 					for (Instrumentor inst : conf.getInstrumentors()) {
 						CompoundLoader bl = new CompoundLoader();
 						bl.addLoader(new MemoryLoader(className, bytecode));
@@ -79,14 +102,59 @@ public class Agent {
 						bytecode = inst.instrument(className, bl,
 								NoopMonitor.INSTANCE);
 					}
-					return bytecode;
 				}
-			} catch (Exception e) {
-				// TODO debug below
-				e.printStackTrace();
-				throw new RuntimeException(e);
+				return bytecode;
+			} catch (InstrumentationException e) {
+				throw new RuntimeException("Error instrumenting class: "
+						+ className, e);
 			}
-			return classfileBuffer;
+		}
+
+		/**
+		 * Merges Salve config using specified class loader and url into
+		 * {@link #config}
+		 * 
+		 * @param loader
+		 *            class loader
+		 * @param url
+		 *            url to salve.xml
+		 */
+		private void mergeConfig(ClassLoader loader, URL url) {
+			try {
+				XmlConfigReader reader = new XmlConfigReader(loader);
+				reader.read(url.openStream(), config);
+			} catch (Exception e) {
+				throw new RuntimeException(
+						"Could not read Salve configuration from: "
+								+ url.toString(), e);
+			}
+		}
+
+		/**
+		 * Searches class loader for any META-INF/salve.xml resources and merges
+		 * them into {@link #config}
+		 * 
+		 * @param loader
+		 *            class loader
+		 */
+		private void mergeConfigs(ClassLoader loader) {
+			Enumeration<URL> urls = null;
+
+			try {
+				urls = loader.getResources("META-INF/salve.xml");
+			} catch (IOException e) {
+				throw new RuntimeException(
+						"Could not search for Salve configuration files using classloader: "
+								+ loader.toString());
+			}
+
+			while (urls.hasMoreElements()) {
+				URL url = urls.nextElement();
+				if (!seenUrls.contains(url)) {
+					mergeConfig(loader, url);
+					seenUrls.add(url);
+				}
+			}
 		}
 	}
 
