a6ea96fd0830ba35150ea9a3cb6fd75a84ef066f
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2009-01-24 07:29:07 +0000

    cleanup wip

--- trunk/salve-expr-validator/src/main/java/salve/expr/checker/Constants.java
+++
@@ -1,9 +0,0 @@
-package salve.expr.checker;
-
-import salve.asmlib.Type;
-
-public interface Constants
-{
-    static final Type PE = Type.getType("Lsalve/expr/PE;");
-    static final Arg[] PE_INIT = new Arg[] { Arg.TYPE, Arg.EXPRESSION, Arg.MODE };
-}
--- trunk/salve-expr-validator/src/main/java/salve/expr/scanner/Expression.java
+++ trunk/salve-expr-validator/src/main/java/salve/expr/scanner/Expression.java
@@ -1,23 +1,39 @@
 package salve.expr.scanner;
 
+import salve.CodeMarker;
 import salve.asmlib.Type;
 
 public class Expression
 {
     private final Type type;
-    private final String expression;
+    private final String path;
     private final String mode;
+    private final CodeMarker location;
 
     public Expression(Type type, String expression, String mode)
     {
-        this.type = type;
-        this.expression = expression;
-        this.mode = mode;
+        this(type, expression, mode, null);
     }
 
-    public String getExpression()
+
+    public Expression(Type type, String expression, String mode, CodeMarker location)
     {
-        return expression;
+        this.type = type;
+        this.path = expression;
+        this.mode = mode;
+        this.location = location;
+    }
+
+
+    public CodeMarker getLocation()
+    {
+        return location;
+    }
+
+
+    public String getPath()
+    {
+        return path;
     }
 
     public String getMode()
@@ -34,8 +50,8 @@ public class Expression
     public String toString()
     {
         return new StringBuilder("[").append(getClass().getSimpleName()).append(" type=").append(
-                type.getInternalName()).append(", expression=").append(expression)
-                .append(", mode=").append(mode).append("]").toString();
+            type.getInternalName()).append(", expression=").append(path).append(", mode=").append(
+            mode).append(", location=").append(location).append("]").toString();
     }
 
 
