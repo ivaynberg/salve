8a12a158a8d5ddc6697a2532e1a5bb5903d06c3a
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2009-01-30 07:58:11 +0000

    added support for boolean getters that start with "is"

--- salve-expr-validator/src/test/java/salve/expr/validator/model/Address.java
+++ salve-expr-validator/src/test/java/salve/expr/validator/model/Address.java
@@ -1,23 +1,32 @@
 package salve.expr.validator.model;
 
-public class Address {
-	private String street;
-	private City city;
+public class Address
+{
+    private String street;
+    private City city;
 
-	public City getCity() {
-		return city;
-	}
+    public City getCity()
+    {
+        return city;
+    }
 
-	public String getStreet() {
-		return street;
-	}
+    public String getStreet()
+    {
+        return street;
+    }
 
-	public void setCity(City city) {
-		this.city = city;
-	}
+    public void setCity(City city)
+    {
+        this.city = city;
+    }
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
 
+    public boolean isResidential()
+    {
+        return false;
+    }
 }
