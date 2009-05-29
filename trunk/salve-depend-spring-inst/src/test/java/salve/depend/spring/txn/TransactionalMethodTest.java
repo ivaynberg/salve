22401e6a658095d8825fcba25819dfd6abab48a2
pete4711 <pete4711@9d424e36-f033-0410-8498-1daa85fe0015>
2009-05-29 21:50:29 +0000

    test proper location of annotations on instrumented @Transactional method

---
+++ trunk/salve-depend-spring-inst/src/test/java/salve/depend/spring/txn/TransactionalMethodTest.java
@@ -0,0 +1,104 @@
+package salve.depend.spring.txn;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.transaction.annotation.Transactional;
+import salve.Scope;
+import salve.depend.DependencyLibrary;
+import salve.depend.Key;
+import salve.depend.Locator;
+import salve.depend.cache.NoopCacheProvider;
+import salve.loader.BytecodePool;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TransactionalMethodTest extends Assert
+{
+	private static String METHODBEAN_NAME = "salve/depend/spring/txn/MethodBean";
+
+	private TestTransactionManager transactionManager;
+	private static Class<?> testClass;
+
+	@BeforeClass
+	public static void initClass() throws Exception
+	{
+		final ClassLoader loader = TestTransactionManager.class.getClassLoader();
+		final BytecodePool pool = new BytecodePool(Scope.ALL).addLoaderFor(loader);
+		final TransactionalInstrumentor inst = new TransactionalInstrumentor();
+		testClass = pool.instrumentIntoClass(METHODBEAN_NAME, inst);
+	}
+
+	@Before
+	public void init()
+	{
+		transactionManager = new TestTransactionManager();
+
+		DependencyLibrary.clear();
+		DependencyLibrary.setCacheProvider(new NoopCacheProvider());
+		DependencyLibrary.addLocator(new Locator()
+		{
+			public Object locate(final Key key)
+			{
+				if (TransactionManager.class.equals(key.getType()))
+					return transactionManager;
+
+				return null;
+			}
+		});
+	}
+
+	@Test
+	public void testMethodByExplicitCall() throws IllegalAccessException, InstantiationException
+	{
+		((MethodBean) testClass.newInstance()).testTransactionIsWorking(transactionManager);
+	}
+
+	@Test
+	public void testMethodByAnnotationLocation() throws IllegalAccessException, InstantiationException, InvocationTargetException
+	{
+		final MethodBean mb = (MethodBean) testClass.newInstance();
+
+		for (Method method : testClass.getMethods())
+			if (method.isAnnotationPresent(MethodIndicator.class))
+				method.invoke(mb, transactionManager);
+	}
+
+	@MethodIndicator
+	@Transactional
+	public void transactionalMethod()
+	{
+		assertTrue("check that transaction manager has been invoked", transactionManager.isInsideTransaction());
+	}
+
+	public static class TestTransactionManager implements TransactionManager
+	{
+		private boolean insideTransaction = false;
+
+		public Object start(final TransactionAttribute attr, final String txnName)
+		{
+			insideTransaction = true;
+			return null;
+		}
+
+		public void finish(final Object txn)
+		{
+		}
+
+		public void finish(final Throwable ex, final Object txn)
+		{
+		}
+
+		public void cleanup(final Object txn)
+		{
+			insideTransaction = false;
+		}
+
+		public boolean isInsideTransaction()
+		{
+			return insideTransaction;
+		}
+	}
+}
