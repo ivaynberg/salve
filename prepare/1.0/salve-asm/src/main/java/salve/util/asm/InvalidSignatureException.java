0e204d60d7837af65da408bc5b7c9304aacd731a
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2009-06-14 16:53:34 +0000

    cleanup after maven

--- prepare/1.0/salve-asm/src/main/java/salve/util/asm/InvalidSignatureException.java
+++
@@ -1,9 +0,0 @@
-package salve.util.asm;
-
-public class InvalidSignatureException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public InvalidSignatureException(String signature) {
-		super("Signature: " + signature + " is not supported");
-	}
-}
--- prepare/1.0/trunk/salve-depend-spring-inst/src/test/java/salve/depend/spring/txn/RollbackException.java
+++
@@ -1,6 +0,0 @@
-package salve.depend.spring.txn;
-
-public class RollbackException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-}
