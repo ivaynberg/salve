79a8c09eb6021a7664a621d97e5ce10090fd35b2
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2009-07-18 07:21:35 +0000

    basic update listener support

--- trunk/salve-inst/src/main/java/salve/model/ProjectModel.java
+++ trunk/salve-inst/src/main/java/salve/model/ProjectModel.java
@@ -1,16 +1,20 @@
 package salve.model;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import salve.BytecodeLoader;
 import salve.asmlib.ClassReader;
+import salve.util.ListMap;
 import salve.util.asm.ClassVisitorAdapter;
 
 public class ProjectModel {
 	private final BytecodeLoader loader;
 
 	private final Map<String, ClassModel> classes = new HashMap<String, ClassModel>();
+
+	private final ListMap<String, UpdateListener> listeners = new ListMap<String, UpdateListener>();
 
 	public ProjectModel(BytecodeLoader loader) {
 		this.loader = loader;
@@ -37,9 +41,23 @@ public class ProjectModel {
 		return loader;
 	}
 
+	public void notifyUpdateListeners(String cn) {
+		Iterator<UpdateListener> it = listeners.get(cn).iterator();
+		while (it.hasNext()) {
+			switch (it.next().updated()) {
+				case REMOVE:
+					it.remove();
+			}
+		}
+		classes.remove(cn);
+	}
+
+	public void register(String cn, UpdateListener listener) {
+		listeners.add(cn, listener);
+	}
+
 	public void update(byte[] bytecode) {
 		ClassReader reader = new ClassReader(bytecode);
 		reader.accept(new ModelUpdateVisitor(this, new ClassVisitorAdapter()), 0);
 	}
-
 }
