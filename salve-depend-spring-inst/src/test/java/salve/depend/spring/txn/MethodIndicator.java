73f915d14734cf19deea643954df4bf9b6d7edf6
igor.vaynberg <igor.vaynberg@9d424e36-f033-0410-8498-1daa85fe0015>
2009-09-30 17:52:03 +0000

    removing more absoleted modules

--- salve-depend-spring-inst/src/test/java/salve/depend/spring/txn/MethodIndicator.java
+++
@@ -1,9 +0,0 @@
-package salve.depend.spring.txn;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-/** Dummy qualifier annot used in tests */
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MethodIndicator {
-}
